287ae6d9fa197eb6b559ee5ce558da76e4445e9d
Dhruva Juloori <djuloori@hawk.iit.edu>
2017-11-18 12:52:05 -0600

    setparameter query working

--- src/dao/Userdao.java
+++ src/dao/Userdao.java
@@ -11,21 +11,18 @@ import javax.persistence.Query;
  */
 
 public class Userdao {
-   //EntityManager emf = (EntityManager) Persistence.createEntityManagerFactory("NewPersistenceUnit");
      public String finduser(String username, String password){
          EntityManagerFactory emf =  Persistence.createEntityManagerFactory("NewPersistenceUnit");
-        EntityManager em = emf.createEntityManager();
+         EntityManager em = emf.createEntityManager();
          em.getTransaction().begin();
-        //System.out.println("i am here");
-        Query q = (Query) em.createNamedQuery("UserEntity.findAll",UserEntity.class);
-       /* q.setParameter(username,username);
-        q.setParameter(password,password);*/
-        UserEntity un = (UserEntity) q.getSingleResult();
-        //System.out.println(un.getUsername());
-        if(username.equals(un.getUsername()) && password.equals(un.getPassword())){
+         Query q = (Query) em.createNamedQuery("UserEntity.Validation",UserEntity.class);
+         q.setParameter("username",username);
+         q.setParameter("password",password);
+         UserEntity un = (UserEntity) q.getSingleResult();
+         if(username.equals(un.getUsername()) && password.equals(un.getPassword())){
             return un.getUsertype();
-        }else{
-            return "failed";
-        }
+         }else{
+             return "failed";
+         }
      }
 }
