f4baca5acb079083a11b0848d737cb2bf5f77e32
Dhruva Juloori <djuloori@hawk.iit.edu>
2017-12-01 23:24:22 -0600

    main class done

--- src/app.java
+++ src/app.java
@@ -15,50 +15,7 @@ import java.util.List;
  */
 public class app {
     public static void main(String args[]) {
-      /*UserEntity ue = new UserEntity();
-        ue.setUsername("Kushal");
 
-        ClassesEntity ce = new ClassesEntity();
-        ce.setClassId("ITMD-518");
-        ce.setClassName("Java");
-
-        ce.setUsr(ue);
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("PersistenceUnit-1");
-        EntityManager em = emf.createEntityManager();
-        em.getTransaction().begin();
-        em.persist(ce);
-        em.getTransaction().commit();
-        em.close();
-      /*
-      ue.setUsername("Kushal");
-
-      ClassesEntity ce = new ClassesEntity();
-      ce.setClassId("ITMD-518");
-      ce.setClassName("Java");
-
-      ClassesEntity ce2 = new ClassesEntity();
-      ce2.setClassId("8907");
-      ce2.setClassName("ITMD-515");
-
-      List<ClassesEntity> classes = new ArrayList<>();
-      classes.add(ce);
-      classes.add(ce2);
-
-      ue.setClasses(classes);
-      EntityManagerFactory emf = Persistence.createEntityManagerFactory("PersistenceUnit-1");
-      EntityManager em = emf.createEntityManager();
-      em.getTransaction().begin();
-      em.persist(ue);
-      em.getTransaction().commit();
-      em.close();*/
-        //Assignmentdao d1 = new Assignmentdao();
-        //File file =
-              //  new File("C:/Users/Sudha/Desktop/project_515.sql");
-      //d1.addClass("ITM","Java","user");
-        //d1.addAssignment("1","Network",100,)
-
-      Classesdao d1 = new Classesdao();
-      d1.removeClass("1");
 
     }
 
